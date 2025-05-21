74bdc757b9d9f5b171255481c8adad406fc5d683
lb15 <lbyrnes13@gmail.com>
2025-05-21 11:26:41 -0700

    updated log output

--- sub_v5autoseurat.sh
+++ sub_v5autoseurat.sh
@@ -1,8 +1,7 @@
 #!/bin/bash                         #-- what is the language of this shell
 #                                  #-- Any line that starts with #$ is an instruction to SGE
 #$ -S /bin/bash                     #-- the shell for the job
-#$ -o /wynton/group/reiter/lauren/log_autoS                        #-- output directory (fill in)
-#$ -e /wynton/group/reiter/lauren/log_autoS                        #-- error directory (fill in)
+#$ -o /dev/null   # Prevent default SGE output
 #$ -cwd                            #-- tell the job that it should start in your working directory
 #$ -r y                            #-- tell the system that if a job crashes, it should be restarted
 #$ -j y                            #-- tell the system that the STDERR and STDOUT should be joined
@@ -24,7 +23,7 @@ fi
 
 ###### ARGUMENTS ######
 
-## fullpath = full path to the folder containing the FASTQs and where the output version analysis directory will be deposited
+## fullpath = full path to the folder containing the cellranger outs folder and where the output version analysis directory will be deposited
 ## version = what version is this? Analysis folder will be named after this variable
 ## do_marks = boolean (TRUE or FALSE) indicating if you want FindAllMarkers to be run.
 ## soupx = boolean (TRUE or FALSE) indicating if you want to run off the soupx corrected matrix. Assumes there is a directory named "soupx_results" in the fullpath folder containing soupx output.
@@ -41,8 +40,22 @@ dir="${fullpath%/*}"
 PROJECT_ROOT="$SGE_O_WORKDIR"
 script="${PROJECT_ROOT}/seurat_processing.R"
 
-echo >&2 $SCRIPT_DIR
-echo >&2 $PROJECT_ROOT
+## LOG directory
+LOG_DIR=$fullpath/log
+mkdir -p "$LOG_DIR"
+LOG_FILE="${LOG_DIR}/autoseuratV5_${JOB_ID}.log"
+
+# Redirect both stdout and stderr to the log file
+exec > "$LOG_FILE" 2>&1
+
+echo >&2 "Input/Output Direcotry ${fullpath}"
+echo >&2 "autoSeuratV5 Direcotry: ${PROJECT_ROOT}"
+echo >&2 "LOG Directory: ${LOG_FILE}"
+echo >&2 "Project name: ${basename}"
+echo >&2 "Version: ${version}"
+echo >&2 "Parameters file: ${para}"
+echo >&2 "Run FindAllMarkers? : ${do_marks}"
+echo >&2 "Run from Soupx corrected matrix? : ${soupx}"
 
 ## move to temp dir and create new folder, copy needed files to TMPDIR
 
